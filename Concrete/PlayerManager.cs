06bc5852b3eec00a394cc3c0e8cef4ffce0cbb15
somer81 <sevinc.omer@gmail.com>
2021-01-27 14:53:44 +0300

    modified

--- Concrete/PlayerManager.cs
+++ Concrete/PlayerManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Game.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace Game.Concrete
 {
-    class PlayerManager
+    class PlayerManager : IGameService
     {
+        private IEntity _entity;
+        public PlayerManager(IEntity entity)
+        {
+            _entity = entity;
+        }
+
+        public void Add()
+        {
+            Console.WriteLine("Player is Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Player is Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Player is Updated");
+        }
     }
 }
